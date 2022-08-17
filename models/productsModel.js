fa903d4b44ed14ec9a784bb6de2841fd36af3df3
davi <daviseles99@gmail.com>
2022-08-17 08:51:55 -0500

    fix: queries

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,14 +1,14 @@
 const connection = require('./connection');
 
 const getProducts = async () => {
-  const query = 'SELECT * FROM products';
+  const query = 'SELECT * FROM StoreManager.products';
   const [result] = await connection.execute(query);
 
   return result;
 };
 
 const getProductById = async (id) => {
-  const query = 'SELECT * FROM products WHERE id = ?';
+  const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
   const [result] = await connection.execute(query, [id]);
 
   return result;
